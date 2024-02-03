63cf402e5f9d0491223b3ffc58e67160fdf21638
VishPrem <91227044+VishPrem@users.noreply.github.com>
2024-02-03 13:33:21 -0500

    Add files via upload

---
+++ diabetes_home.py
@@ -0,0 +1,27 @@
+# Show complete dataset and summary in 'diabetes_home.py'
+# Import the streamlit modules.
+import streamlit as st
+
+# Define a function 'app()' which accepts 'diabetes_df' as an input.
+def app(diabetes_df):
+    # Set the title to the home page contents.
+    st.title('Early Diabetes Prediction Web App')
+    # Provide a brief description for the web app.
+    st.markdown("Diabetes is a chronic (long-lasting) health condition that affects how your body turns food into energy. There isn’t a cure yet for diabetes, but losing weight, eating healthy food, and being active can really help in reducing the impact of diabetes. This Web app will help you to predict whether a person has diabetes or is prone to get diabetes in future by analysing the values of several features using the Decision Tree Classifier.")
+    # Add the 'beta_expander' to view full dataset 
+    st.subheader('View Data')
+    with st.beta_expander('View Data'):
+      st.dataframe(diabetes_df)
+    # Add a checkbox in the first column. Display the column names of 'diabetes_df' on the click of checkbox.
+    col_1, col_2, col_3 = st.beta_columns(3)
+    with col_1:
+      if st.checkbox('Show all column names'):
+        st.table(diabetes_df.columns)
+    # Add a checkbox in the second column. Display the column data-types of 'diabetes_df' on the click of checkbox.
+    with col_2:
+      if st.checkbox('View column data-type'):
+        st.table(diabetes_df.dtypes)
+    # Add a checkbox in the third column followed by a selectbox which accepts the column name whose data needs to be displayed.
+    with col_3:
+      col = st.selectbox('Select column', tuple(diabetes_df.columns))
+      st.write(diabetes_df[col])      
---
+++ diabetes_main.py
@@ -0,0 +1,42 @@
+# Importing the necessary Python modules.
+import streamlit as st
+import numpy as np
+import pandas as pd
+import diabetes_predict
+import diabetes_home
+import diabetes_plots
+
+# Configure your home page by setting its title and icon that will be displayed in a browser tab.
+st.set_page_config(page_title = 'Early Diabetes Prediction Web App',
+                    page_icon = 'random',
+                    layout = 'wide',
+                    initial_sidebar_state = 'auto'
+                    )
+
+# Loading the dataset.
+@st.cache()
+def load_data():
+    # Load the Diabetes dataset into DataFrame.
+
+    df = pd.read_csv('b510b80d-2fd6-4c08-bfdf-2a24f733551d.csv')
+    df.head()
+
+    # Rename the column names in the DataFrame.
+    df.rename(columns = {"BloodPressure": "Blood_Pressure",}, inplace = True)
+    df.rename(columns = {"SkinThickness": "Skin_Thickness",}, inplace = True)
+    df.rename(columns = {"DiabetesPedigreeFunction": "Pedigree_Function",}, inplace = True)
+
+    df.head() 
+
+    return df
+
+diabetes_df = load_data()
+
+# Adding a navigation in the sidebar using radio buttons
+# Create the 'pages_dict' dictionary to navigate.
+pages_dict = {"Home": diabetes_home, "Predict Diabetes": diabetes_predict, "Visualise Decision Tree": diabetes_plots}
+st.sidebar.title('Navigation')
+# Add radio buttons in the sidebar for navigation and call the respective pages based on user selection.
+user_choice = st.sidebar.radio('Go to', pages_dict.keys())
+selected_page = pages_dict[user_choice]
+selected_page.app(diabetes_df)
