c93f0ad7e77a961cbd0334d258cd11e6781af71d
VishPrem <91227044+VishPrem@users.noreply.github.com>
2024-02-03 13:57:09 -0500

    Update diabetes_home.py

--- diabetes_home.py
+++ diabetes_home.py
@@ -10,8 +10,8 @@ def app(diabetes_df):
     st.markdown("Diabetes is a chronic (long-lasting) health condition that affects how your body turns food into energy. There isn’t a cure yet for diabetes, but losing weight, eating healthy food, and being active can really help in reducing the impact of diabetes. This Web app will help you to predict whether a person has diabetes or is prone to get diabetes in future by analysing the values of several features using the Decision Tree Classifier.")
     # Add the 'beta_expander' to view full dataset 
     st.subheader('View Data')
-    with st.beta_expander('View Data'):
-      st.dataframe(diabetes_df)
+    # with st.beta_expander('View Data'):
+    #   st.dataframe(diabetes_df)
     # Add a checkbox in the first column. Display the column names of 'diabetes_df' on the click of checkbox.
     col_1, col_2, col_3 = st.beta_columns(3)
     with col_1:
